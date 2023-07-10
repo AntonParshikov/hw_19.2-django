cb0b7bae3a2f3794aeafa0d014d5625c36f0015d
Anton Parshikov <tosha1703@gmail.com>
2023-07-10 16:15:02 +0300

    add dark theme

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from catalog.views import home, contacts
 from . import views
 
-
 urlpatterns = [
     path('', home),
     path('contacts/', views.contacts, name='contacts')
